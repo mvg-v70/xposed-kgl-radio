5462911711478a04ab3d2a94e16fa07edf4c2f1c
mvg-v70 <gusev-mv@yandex.ru>
2015-10-01 19:55:53 +0300

    version 1.1

--- src/com/mvgv70/xposed_kgl_radio/Main.java
+++ src/com/mvgv70/xposed_kgl_radio/Main.java
@@ -1,8 +1,11 @@
 package com.mvgv70.xposed_kgl_radio;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,34 +14,66 @@ import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.widget.Button;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 
-public class Main implements IXposedHookLoadPackage 
+public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResources
 {
-	
+
   private static Activity mtcRadio;
   private static OnClickListener mUi;
+  private static int btn_search_id;
   private final static String TAG = "xposed-kgl-radio";
+  
+  @Override
+  public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable 
+  {
+	// load resource
+	XC_LayoutInflated loadPackage = new XC_LayoutInflated() {
+		
+	  @Override
+	  public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable 
+	  {
+	    Log.d(TAG,"handleLayoutInflated");
+	    btn_search_id = liparam.res.getIdentifier("btn_search", "id", "com.microntek.radio");
+	    Log.d(TAG,"btn_search="+btn_search_id);
+      }
+	};
+	  
+    if (!resparam.packageName.equals("com.microntek.radio")) return;
+	resparam.res.hookLayout("com.microntek.radio", "layout", "radio", loadPackage);
+    Log.d(TAG,"com.microntek.radio resource hook OK");
+  }
   
   @Override
   public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable 
   {
-    // MTCRadio.onCreate(Bindle)
+    // RadioActivity.onCreate(Bindle)
     XC_MethodHook onCreate = new XC_MethodHook() {
 	           
       @Override
       protected void afterHookedMethod(MethodHookParam param) throws Throwable {
     	Log.d(TAG,"onCreate");
       	mtcRadio = ((Activity)param.thisObject);
+        // показать версию модуля
+        try 
+        {
+     	  Context context = mtcRadio.createPackageContext(getClass().getPackage().getName(), Context.CONTEXT_IGNORE_SECURITY);
+     	  String version = context.getString(R.string.app_version_name);
+          Log.d(TAG,"version="+version);
+     	} catch (NameNotFoundException e) {}
       	// кнопка поиска
-      	Button btnSearch = (Button)mtcRadio.findViewById(0x7f05001e);
-      	mUi = (OnClickListener)XposedHelpers.getObjectField(param.thisObject, "mUi");
-      	// убираем обработчик нажатия
-      	btnSearch.setOnClickListener(null);
-      	btnSearch.setClickable(false);
-      	// устанавливаем обработчик длинного нажатия
-      	btnSearch.setLongClickable(true);
-      	btnSearch.setOnLongClickListener(searchLongClick);
-      	Log.d(TAG,"btnSearch remap OK");
+      	Button btnSearch = (Button)mtcRadio.findViewById(btn_search_id);
+      	if (btnSearch != null)
+      	{
+      	  // убираем обработчик нажатия
+      	  btnSearch.setOnClickListener(null);
+          btnSearch.setClickable(false);
+          // устанавливаем обработчик длинного нажатия
+      	  btnSearch.setLongClickable(true);
+          btnSearch.setOnLongClickListener(searchLongClick);
+      	  Log.d(TAG,"btnSearch remap OK");
+      	}
       }
     };
     
@@ -48,7 +83,7 @@ public class Main implements IXposedHookLoadPackage
     XposedHelpers.findAndHookMethod("com.microntek.radio.RadioActivity", lpparam.classLoader, "onCreate", Bundle.class, onCreate);
     Log.d(TAG,"com.microntek.radio hook OK");
   }
-
+  
   // обработчик длинного нажатия на кнопку поиска
   private OnLongClickListener searchLongClick = new OnLongClickListener()
   {
